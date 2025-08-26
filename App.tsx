a8e271b5163125a307ce8f515cf1a1bdb8859b3d
Emmanuel <preciousemmanuel32@gmail.com>
2025-08-26 18:43:21 +0100

    first push

---
+++ App.tsx
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { LogBox,View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+
+// import RNCallKeep from 'react-native-callkeep';
+
+
+function App(): React.JSX.Element {
+
+  
+
+ 
+  return (
+    <View>
+
+      <Text>Hii</Text>
+    </View>
+  );
+}
+
+
+
+export default App;
